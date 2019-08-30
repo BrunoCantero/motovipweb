e0763824c910bd9f2ed195f37738e97e740ec927
alexis Quiñonez <alexisquionez21@gmail.com>
2019-08-30 14:19:58 -0300

    modelo del modulo pedidos

--- src/components/ListPedidos.js
+++ src/components/ListPedidos.js
@@ -1,58 +1,114 @@
 /* eslint-disable no-useless-constructor */
 import React ,{Component} from 'react';
 import { Modal,Button}   from 'react-bootstrap';
+import Calendar from 'react-calendar';
+import moment             from 'moment';
+
+moment.locale('es');
 
 class ListPedidos extends Component{
 
     constructor(props){
         super(props);
+        this.hiddenModalNuevoPedido = this.hiddenModalNuevoPedido.bind(this);
+        this.state={
+            showModalNuevoPedido:false,
+            showFecha : false,
+            date: new Date(),
+            fechaSeleccionada:''
+        }
     }
 
+
+    hiddenModalNuevoPedido(){
+        this.setState({
+            showModalNuevoPedido:false
+        })
+    }
+
+    modalNuevoPedido(){
+        this.setState({
+            showModalNuevoPedido:true
+        })
+    }
+
+    mostrarCalendario(){
+        alert("aasd")
+        this.setState({
+            showFecha : !this.state.showFecha
+        })
+    }
+
+    onChangeFechaSeleccionada (date){
+        var fecha_seleccionada = moment(date).format('YYYY-MM-DD');
+        this.setState({ fechaSeleccionada:fecha_seleccionada,showFecha:false })
+    }
+
+   
     
     render(){
         return(
            <div className="content">
                 <div className="container-fluid">
                     <div className="row">
+                        <div className="col-md-4">
+                            <div class="form-group" style={{marginBottom:10}}>
+                                <input type="text" class="form-control" name="fecha" onClick={()=>this.mostrarCalendario()} value={this.state.fechaSeleccionada} placeholder="Fecha.." style={{width:280}} />
+                                {this.state.showFecha &&
+                                    <Calendar
+                                        onChange={this.onChangeFechaSeleccionada.bind(this)}
+                                        value={this.state.date}
+                                    />
+                                } 
+                            </div>   
+                            
+                        </div>
+                        <div className="col-md-4">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="fecha"   placeholder="Clientes.." style={{width:280}} />
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <select class="form-control" name="fecha" style={{float:'left',width:260}}>
+                                    <option>Elegir</option>
+                                </select>
+                                <Button variant="btn btn-info" style={{float:'right'}} onClick={()=>this.modalNuevoPedido()}> Nuevo pedido</Button>
+                            </div>
+                        </div>    
                         <div className="col-md-12">
                             <div class="card strpied-tabled-with-hover">
                                 <div class="card-body table-full-width table-responsive" id="printablediv">
                                     <table  className="table table-hover table-striped" >
                                         <thead>
                                             <tr>
-                                                <th>#</th>
                                                 <th>Cliente</th>
-                                                <th>Fecha</th>
-                                                <th>Tipo</th>
-                                                <th>Total</th>
+                                                <th>Direccion</th>
+                                                <th>Estado</th>
+                                                <th>Cadete</th>
+                                                <th>Inicio</th>
+                                                <th>Llegada</th>
+                                                <th>Fin</th>
+                                                <th>Total_compra</th>
+                                                <th>Comision_empresa</th>
+                                                <th>Comision_cadete</th>
                                                 <th>Detalle</th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <tr>
-                                                <td>1</td>
-                                                <td><strong>Alexis quiñonez</strong></td>
-                                                <td>19/05/2018 18:30:00</td>
-                                                <td>2hs</td>
-                                                <td>$120</td>
+                                                <td>Cliente Daasaddsds</td>
+                                                <td>Direccion</td>
+                                                <td>Estado</td>
+                                                <td>Cadete</td>
+                                                <td>Inicio</td>
+                                                <td>Llegada</td>
+                                                <td>Fin</td>
+                                                <td>Total_compra</td>
+                                                <td>$445</td>
+                                                <td>$44545445</td>
                                                 <td><center><Button bsStyle="primary"> VER</Button></center></td>
                                             </tr>
-                                            <tr>
-                                                <td>1</td>
-                                                <td><strong>Gonzalez marcos</strong></td>
-                                                <td>19/05/2018 18:30:00</td>
-                                                <td>2hs</td>
-                                                <td>$120</td>
-                                                <td><center><Button bsStyle="primary"> VER</Button></center></td>
-                                            </tr>
-                                            <tr>
-                                                <td>1</td>
-                                                <td><strong>Bruno cantero</strong></td>
-                                                <td>19/05/2018 18:30:00</td>
-                                                <td>2hs</td>
-                                                <td>$120</td>
-                                                <td><center><Button bsStyle="primary"> VER</Button></center></td>
-                                            </tr>    
                                         </tbody>        
                                     </table>
                                 </div>
@@ -60,7 +116,18 @@ class ListPedidos extends Component{
                         </div>
                     </div>
                 </div>
+                <Modal  show={this.state.showModalNuevoPedido}  size="lg" onHide={this.hiddenModalNuevoPedido} >
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            <strong>Nuevo pedido</strong>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body> 
+                        sadsdadasd
+                    </Modal.Body>
+                </Modal>  
             </div>
+                
         )
     }
 }
