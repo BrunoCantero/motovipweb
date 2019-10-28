6c14edb5e8959d03a6fd50cb319ba4449d1f56ea
Bruno Cantero <brunocantero01@gmail.com>
2019-10-28 14:11:25 -0300

    last build, reporte por ganancias

---
+++ build/precache-manifest.e10e30e4d68e680a0b4515867cf6ca4f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f8ad677ce1f0ffecc3bd03f355f07b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0547900f8d0eba16bc1c",
+    "url": "/static/css/2.5e42defe.chunk.css"
+  },
+  {
+    "revision": "e40011d3fd1da651c026",
+    "url": "/static/css/main.cc2fdf1e.chunk.css"
+  },
+  {
+    "revision": "0547900f8d0eba16bc1c",
+    "url": "/static/js/2.a2d75c50.chunk.js"
+  },
+  {
+    "revision": "e40011d3fd1da651c026",
+    "url": "/static/js/main.1642c421.chunk.js"
+  },
+  {
+    "revision": "f8124cd7cd572987bc22",
+    "url": "/static/js/runtime~main.910f2179.js"
+  },
+  {
+    "revision": "ec5c4be47d384de707974a9be78df0cb",
+    "url": "/static/media/loading.ec5c4be4.gif"
+  },
+  {
+    "revision": "1bb42ed5b9a982d0edb6a3b0802fe86a",
+    "url": "/static/media/logo.1bb42ed5.jpg"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/static/media/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "861720fa94b80958075f620b2c6e8fb9",
+    "url": "/static/media/nucleo-icons.861720fa.woff"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "/static/media/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/static/media/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/static/media/nucleo-icons.ff208b8d.woff2"
+  }
+]);
